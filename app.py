0939aed41b6720387bfb7b621df343f2d8b44957
Fatih Akgüç <fatihakguc@Admins-MacBook-Pro.local>
2025-02-11 16:56:49 -0600

    Initial Commit

--- app.py
+++ app.py
@@ -4,41 +4,28 @@ import time
 
 st.header('Tossing a Coin')
 
-# Store progress in a list
-progress = []
-
-# Create a line chart
-chart = st.line_chart(progress)
+chart = st.line_chart([0.5])
 
 def toss_coin(n):
+
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
-    outcome_1_count = 0
+    mean = None
     outcome_no = 0
+    outcome_1_count = 0
 
-    # Loop through the outcomes and update chart data
     for r in trial_outcomes:
-        outcome_no += 1
+        outcome_no +=1
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
-        
-        # Append the mean to the progress list for chart update
-        progress.append(mean)
-        
-        # Redraw the chart with updated progress
-        chart.line_chart(progress)
-        
-        # Add a slight delay for smooth animation
+        chart.add_rows([mean])
         time.sleep(0.05)
 
     return mean
 
-# Add widgets
 number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
 start_button = st.button('Run')
 
 if start_button:
-    st.write(f'Running the experiment of {number_of_trials} trials.')
-    toss_coin(number_of_trials)
-    toss_coin(number_of_trials)
+    st.write(f'Running the experient of {number_of_trials} trials.')
